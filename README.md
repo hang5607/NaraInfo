6fa79e7fddf2fc1efd1f5519c9c3c8db176543ba
hang5607 <hang5607@naver.com>
2020-03-30 21:36:44 +0900

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# project1
+프로젝트
+# Git-hospitalService
+서비스!!
---
+++ src/main/java/dg/service/project1/controller/MainController.java
@@ -0,0 +1,19 @@
+package dg.service.project1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * MainController
+ */
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String index(){
+        return "HELLO";
+    }
+    
+}
